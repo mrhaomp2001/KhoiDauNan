8706e96d79ac5543d8252d8c0a551c9361e27fff
mrhaomp2001 <huynhnhuthao248@gmail.com>
2023-03-12 20:55:56 +0700

    add save system

---
+++ Assets/Scripts/Save System/PlayerData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    private int lv;
+    public PlayerData (PlayerController playerController)
+    {
+        lv = playerController.Lv;
+    }
+
+    public int Lv { get => lv; set => lv = value; }
+}
---
+++ Assets/Scripts/Save System/SaveSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    public static void SavePlayer(PlayerController playerController)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/save.sav";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(playerController);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+    public static PlayerData LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/save.sav";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
